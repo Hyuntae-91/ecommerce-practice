04b9497507938be526f1a125e057772a9223f9bc
Hyuntae <dev.hyuntae@gmail.com>
2025-04-10 00:26:19 +0900

    docs: 설계변경

--- src/main/java/kr/ecommerce/be/server/ServerApplication.java
+++ src/main/java/kr/ecommerce/be/server/ServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "kr.ecommerce.be.server.infrastructure.point")
-@EntityScan(basePackages = "kr.ecommerce.be.server.domain.point.model")
+@EnableJpaRepositories(basePackages = "kr.ecommerce.be.server.infrastructure")
+@EntityScan(basePackages = "kr.ecommerce.be.server.domain")
 @SpringBootApplication
 public class ServerApplication {
 
