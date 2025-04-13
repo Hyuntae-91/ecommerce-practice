d89eb0f5713fdf46c3a4a104daf858d7ba554344
Hyuntae <dev.hyuntae@gmail.com>
2025-04-13 21:47:04 +0900

    rollback. 4주차 관련 코드 삭제

--- src/main/java/kr/ecommerce/be/server/infrastructure/order/repository/OrderJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ecommerce.be.server.infrastructure.order.repository;
-
-import kr.ecommerce.be.server.domain.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderJpaRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/kr/ecommerce/be/server/infrastructure/point/repository/UserPointJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ecommerce.be.server.infrastructure.point.repository;
-
-import kr.ecommerce.be.server.domain.point.model.UserPoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPointJpaRepository extends JpaRepository<UserPoint, Long> {
-}
