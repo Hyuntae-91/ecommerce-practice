32c1740a4d4cfb42e9e804913f0050891b93f20d
Hyuntae <dev.hyuntae@gmail.com>
2025-04-17 15:34:51 +0900

    feat: 주문 Infrastructure 코드 개발 및 EtoE 테스트코드 추가

---
+++ src/main/java/kr/ecommerce/be/server/infrastructure/order/repository/OrderItemJpaRepository.java
@@ -0,0 +1,13 @@
+package kr.ecommerce.be.server.infrastructure.order.repository;
+
+import kr.ecommerce.be.server.domain.order.model.OrderItem;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderItemJpaRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrderId(Long orderId);
+
+    List<OrderItem> findCartByUserId(Long userId);
+}
