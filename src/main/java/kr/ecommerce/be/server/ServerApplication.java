94a052ea035366e6a7d7f8cc82622b9c0f3664bd
Hyuntae <dev.hyuntae@gmail.com>
2025-04-25 01:35:19 +0900

    feat: scheduler 추가. 쿠폰만료 처리

--- src/main/java/kr/ecommerce/be/server/ServerApplication.java
+++ src/main/java/kr/ecommerce/be/server/ServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaRepositories(basePackages = "kr.ecommerce.be.server.infrastructure")
 @EntityScan(basePackages = "kr.ecommerce.be.server.domain")
 @SpringBootApplication
+@EnableScheduling
 public class ServerApplication {
 
 	public static void main(String[] args) {
