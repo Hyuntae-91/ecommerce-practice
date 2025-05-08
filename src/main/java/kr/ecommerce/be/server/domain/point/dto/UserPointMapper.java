a69919020fe325c6c13942f7f1c5f1e22cf888ff
Hyuntae <dev.hyuntae@gmail.com>
2025-05-08 21:24:49 +0900

    refactor: 결제 서비스 간소화, 포인트 사용 책임 userPoint 로 이관, 그외 코드 리팩토링

--- src/main/java/kr/ecommerce/be/server/domain/point/dto/UserPointMapper.java
+++ src/main/java/kr/ecommerce/be/server/domain/point/dto/UserPointMapper.java
@@ -2,6 +2,7 @@ package kr.ecommerce.be.server.domain.point.dto;
 
 import kr.ecommerce.be.server.domain.point.dto.response.PointChargeServiceResponse;
 import kr.ecommerce.be.server.domain.point.dto.response.PointHistoryServiceResponse;
+import kr.ecommerce.be.server.domain.point.dto.response.PointUseServiceResponse;
 import kr.ecommerce.be.server.domain.point.dto.response.UserPointServiceResponse;
 import kr.ecommerce.be.server.domain.point.model.PointHistory;
 import kr.ecommerce.be.server.domain.point.model.UserPoint;
@@ -18,6 +19,8 @@ public interface UserPointMapper {
     UserPointServiceResponse toUserPointResponse(UserPoint userPoint);
 
     PointChargeServiceResponse toUserPointChargeResponse(UserPoint userPoint);
+
+    PointUseServiceResponse toUserPointUseResponse(UserPoint userPoint);
 
     PointHistoryServiceResponse toUserPointHistoryResponse(PointHistory pointHistory);
 
