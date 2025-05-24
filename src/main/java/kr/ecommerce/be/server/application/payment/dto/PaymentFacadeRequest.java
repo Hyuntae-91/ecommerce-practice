341e5885ade14ebab2d46b8c12c4bf900f7d4076
Hyuntae <dev.hyuntae@gmail.com>
2025-05-24 10:06:40 +0900

    refactor: Payment Facade 삭제처리

--- src/main/java/kr/ecommerce/be/server/application/payment/dto/PaymentFacadeRequest.java
+++
@@ -1,9 +0,0 @@
-package kr.ecommerce.be.server.application.payment.dto;
-
-import java.util.List;
-
-public record PaymentFacadeRequest (
-        Long userId,
-        List<PaymentProductFacadeDto> products,
-        Long couponId
-) {}
